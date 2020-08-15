308e3502f7924cbc8208baa6e5627164e16d12f7
L1STOV <listovspartak99@gmail.com>
2020-08-15 11:43:42 +0300

    Realized speach module for helper

--- main.py
+++ main.py
@@ -13,15 +13,15 @@ r = sr.Recognizer()
 def record_audio(ask = False):
     with sr.Microphone() as source:
         if ask:
-            print(ask)
+            fox_speak(ask)
         audio = r.listen(source)
         voice_data = ''
         try:
             voice_data = r.recognize_google(audio)
         except sr.UnknownValueError:
-            print("Sorry, didn't get that")
+            fox_speak("Sorry, didn't get that")
         except sr.RequestError:
-            print("Check your Internet connection")
+            fox_speak("Check your Internet connection")
         return voice_data
 
 
@@ -38,27 +38,27 @@ def fox_speak(audio_string):
 def respond(voice_data):
     now = datetime.datetime.now()
     if 'what is your name' in voice_data:
-        print("My name is Fox")
+        fox_speak("My name is Fox")
     if 'what time is now' in voice_data:
-        print("Now " + str(now.hour) + ":" + str(now.minute))
+        fox_speak("Now " + str(now.hour) + ":" + str(now.minute))
     if 'what date is today' in voice_data:
-        print("Today is " + str(now.day) + " of " + str(now.month))
+        fox_speak("Today is " + str(now.day) + " of " + str(now.month))
     if 'search' in voice_data:
         search = record_audio("What do you want to search")
         url = "https://google.com/search?q=" + search
         webbrowser.get().open(url)
-        print("Here is what i found for " + search)
+        fox_speak("Here is what i found for " + search)
     if 'find location' in voice_data:
         location = record_audio("What's the location")
         url = "https://google.nl/maps/place/" + location + '/&amp;'
         webbrowser.get().open(url)
-        print("Here is the location " + location)
+        fox_speak("Here is the location " + location)
     if 'exit' in voice_data:
         exit()
 
 
 time.sleep(1)
-print("How can i help you?")
+fox_speak("How can i help you?")
 while 1:
     voice_data = record_audio()
     respond(voice_data)
