abc76932c8734565a3c304f992477094faa5e5a4
L1STOV <listovspartak99@gmail.com>
2020-08-15 11:38:46 +0300

    Implement gTTS module to voice helper

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import speech_recognition as sr
 import datetime
 import webbrowser
+import time
+from gtts import gTTS
+import playsound
+import os
+import random
 
 r = sr.Recognizer()
 
@@ -20,6 +25,16 @@ def record_audio(ask = False):
         return voice_data
 
 
+def fox_speak(audio_string):
+    tts = gTTS(text=audio_string, lang='en')
+    r = random.randint(1, 10000000)
+    audio_file = 'audio-' + str(r) + '.mp3'
+    tts.save(audio_file)
+    playsound.playsound(audio_file)
+    print(audio_string)
+    os.remove(audio_file)
+
+
 def respond(voice_data):
     now = datetime.datetime.now()
     if 'what is your name' in voice_data:
@@ -34,12 +49,16 @@ def respond(voice_data):
         webbrowser.get().open(url)
         print("Here is what i found for " + search)
     if 'find location' in voice_data:
-        location = record_audio("What's the location'")
+        location = record_audio("What's the location")
         url = "https://google.nl/maps/place/" + location + '/&amp;'
         webbrowser.get().open(url)
         print("Here is the location " + location)
+    if 'exit' in voice_data:
+        exit()
 
 
+time.sleep(1)
 print("How can i help you?")
-voice_data = record_audio()
-respond(voice_data)
+while 1:
+    voice_data = record_audio()
+    respond(voice_data)
